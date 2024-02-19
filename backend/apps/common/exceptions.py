17346b8aca2415b4006b029fad998b7dd5b46c47
SerMeliodas <oliver9574@gmail.com>
2024-02-19 16:15:12 +0100

    fix(common): super super()

--- backend/apps/common/exceptions.py
+++ backend/apps/common/exceptions.py
@@ -6,6 +6,6 @@ class AlreadyExistError(Exception):
 
     def __init__(self, instance: Model):
         self.message = f"{instance.__class__.__name__} already have \
-        the same instance in db"
+the same instance in db"
 
-        super.__init__(self.message)
+        super().__init__(self.message)
