d820dcf30c39c49689448e64301eabab27acdfb3
SerMeliodas <oliver9574@gmail.com>
2024-02-19 11:09:59 +0100

    refactor(novels): some pep8 refactors

--- backend/apps/common/services.py
+++ backend/apps/common/services.py
@@ -20,7 +20,8 @@ def model_update(*, instance: models.Model, fields: list[str],
     has_updated = False
     updated_fields = list()
     many_to_many_fields = dict()
-    model_fields = {field.name: field for field in instance._meta.fields + instance._meta.many_to_many}
+    model_fields = {field.name: field for field in instance._meta.fields
+                    + instance._meta.many_to_many}
 
     for field in fields:
         if field not in data:
@@ -28,7 +29,8 @@ def model_update(*, instance: models.Model, fields: list[str],
 
         model_field = model_fields.get(field)
 
-        assert model_field is not None, f"{field} is not part of {instance.__class__.__name__} fields"
+        assert model_field is not None, f"{field} is not part of \
+        {instance.__class__.__name__} fields"
 
         if isinstance(model_fields[field], models.ManyToManyField):
             many_to_many_fields[field] = data[field]
@@ -41,7 +43,8 @@ def model_update(*, instance: models.Model, fields: list[str],
 
     if has_updated:
         if auto_updated_at:
-            if "updated_at" in model_fields and "updated_at" not in updated_fields:
+            if ("updated_at" in model_fields and
+                    "updated_at" not in updated_fields):
                 updated_fields.append("updated_at")
                 instance.updated_at = timezone.now()
 
