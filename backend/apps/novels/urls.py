80cb8211dd3d29344f397e3449493f8063ca02b2
SerMeliodas <oliver9574@gmail.com>
2024-02-21 13:58:30 +0100

    feat(genre): genre url dispatcher endpoints

--- backend/apps/novels/urls.py
+++ backend/apps/novels/urls.py
@@ -18,6 +18,26 @@ from apps.novels.apis.tag import (
 )
 
 
+from apps.novels.apis.genre import (
+    GenreGetApi,
+    GenreListApi,
+    GenreUpdateApi,
+    GenreDeleteApi,
+    GenreCreateApi
+)
+
+
+genre_patterns = [
+    path('create/', GenreCreateApi.as_view(), name='create new genre'),
+    path('delete/<int:pk>/', GenreDeleteApi.as_view(),
+         name='delete genre instance'),
+    path('update/<int:pk>/', GenreUpdateApi.as_view(),
+         name='update genre instance'),
+    path('', GenreListApi.as_view(), name='genre list'),
+    path('<int:pk>/', GenreGetApi.as_view(), name='get genre')
+]
+
+
 tag_patterns = [
     path('create/', TagCreateApi.as_view(), name='create new tag'),
     path('delete/<int:pk>/', TagDeleteApi.as_view(),
@@ -43,4 +63,5 @@ novel_patterns = [
 urlpatterns = [
     path('novels/', include((novel_patterns, 'novels'))),
     path('tags/', include((tag_patterns, 'tags'))),
+    path('genres/', include((genre_patterns, 'genres'))),
 ]
