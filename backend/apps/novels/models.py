ec247e4cab886655b9f1ddc922db6067c16ca603
SerMeliodas <oliver9574@gmail.com>
2024-02-24 11:46:10 +0100

    feat(novels, common): BaseModel moved to common models

--- backend/apps/novels/models.py
+++ backend/apps/novels/models.py
@@ -1,16 +1,8 @@
 from django.db import models
-from django.utils import timezone
 from django.utils.text import slugify
 
 from apps.common.exceptions import AlreadyExistError
-
-
-class BaseModel(models.Model):
-    created_at = models.DateTimeField(db_index=True, default=timezone.now)
-    updated_at = models.DateTimeField(db_index=True, auto_now=True)
-
-    class Meta:
-        abstract = True
+from apps.common.models import BaseModel
 
 
 class Novel(BaseModel):
@@ -33,17 +25,6 @@ class Novel(BaseModel):
             raise AlreadyExistError(obj)
         except self.DoesNotExist:
             pass
-
-
-class Chapter(BaseModel):
-    title = models.CharField(max_length=255)
-    novel = models.ForeignKey("Novel", on_delete=models.CASCADE)
-    text = models.TextField()
-
-    class Meta:
-        db_table = "chapters"
-        default_related_name = "chapters"
-        ordering = ["novel"]
 
 
 class Tag(models.Model):
