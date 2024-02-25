3faec565e0a516c9a9b48dc40f8cc1fbaa66898c
SerMeliodas <oliver9574@gmail.com>
2024-02-25 23:39:42 +0100

    feat(chapters): deleted get_chapters_list() service

--- backend/apps/chapters/selectors.py
+++ backend/apps/chapters/selectors.py
@@ -2,12 +2,9 @@ from .models import Chapter
 from apps.novels.models import Novel
 
 
-def get_chapters_list_by_novel(novel: Novel) -> Chapter:
+def get_chapters_list_by_novel(novel_id: int) -> Chapter:
+    novel = Novel.objects.get(pk=novel_id)
     return Chapter.objects.all().filter(novel=novel)
-
-
-def get_chapters_list() -> Chapter:
-    return Chapter.objects.all()
 
 
 def get_chapter_by_id(pk: int) -> Chapter:
