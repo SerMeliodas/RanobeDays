0487eb0b692912c2f78a6d9a88df78715caa8666
SerMeliodas <oliver9574@gmail.com>
2024-03-23 22:59:13 +0100

    feat(translator_teams): app base setup
    Apis without implementation, admin register, file services, base model

--- backend/apps/translator_teams/apps.py
+++ backend/apps/translator_teams/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class TranslatorsConfig(AppConfig):
+class translator_teamsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.translators'
+    name = 'apps.translator_teams'
