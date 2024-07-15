ff216da9ec1362afb02c0cadd48b1afd4f22dbfb
SerMeliodas <oliver9574@gmail.com>
2024-07-15 18:06:55 +0200

    feat(teams): added two new fields team_type, description

--- backend/apps/teams/models.py
+++ backend/apps/teams/models.py
@@ -3,9 +3,17 @@ from django.contrib.auth import get_user_model
 
 
 class Team(models.Model):
+
+    TEAM_TYPES = {
+        ("A", "Autor"),
+        ("T", "Translator")
+    }
+
     name = models.CharField(max_length=255)
     users = models.ManyToManyField(get_user_model())
     novels = models.ManyToManyField("novels.Novel")
+    team_type = models.CharField(max_length=1, choices=TEAM_TYPES, default="A")
+    description = models.TextField(null=True)
 
     class Meta:
         db_table = 'teams'
