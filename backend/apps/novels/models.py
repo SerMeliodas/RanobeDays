265afe8143375b11b2a5c98bda86b67347ae289e
SerMeliodas <oliver9574@gmail.com>
2024-07-16 14:12:31 +0200

    feat(novel): added new fields
    Status, synopsys, language, translate_language, original_title

--- backend/apps/novels/models.py
+++ backend/apps/novels/models.py
@@ -1,15 +1,33 @@
 from django.db import models
 from django.utils.text import slugify
+from django.utils.translation import gettext_lazy as _
 
 from apps.common.exceptions import AlreadyExistError
 from apps.common.models import BaseModel
 
 
 class Novel(BaseModel):
+    STATUS = (
+        (1, _("Continues")),
+        (2, _("Finished")),
+        (3, _("Frozen"))
+    )
+
     title = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, default=title, db_index=True)
+    original_title = models.CharField(max_length=255, null=True)
+    languages = models.ForeignKey("Languages", db_index=True,
+                                  on_delete=models.DO_NOTHING,
+                                  related_name="novels",
+                                  default=None)
+    translate_language = models.ForeignKey("Languages", db_index=True,
+                                           on_delete=models.DO_NOTHING,
+                                           related_name="translated_novels",
+                                           default=None)
+    status = models.IntegerField(choices=STATUS, default=1)
     tags = models.ManyToManyField("Tag", db_index=True)
     genres = models.ManyToManyField("Genre", db_index=True)
+    synopsys = models.TextField(null=True)
 
     class Meta:
         ordering = ["-created_at"]
@@ -48,6 +66,33 @@ class Genre(models.Model):
 
     def clean(self):
         instance = Genre.objects.filter(name=self.name)
+
+        if instance.exists():
+            raise AlreadyExistError(self)
+
+
+class Country(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = "countries"
+
+    def clean(self):
+        instance = Country.objects.filter(name=self.name)
+
+        if instance.exists():
+            raise AlreadyExistError(self)
+
+
+class Languages(models.Model):
+    name = models.CharField(max_length=50)
+    abbrevation = models.CharField(max_length=5)
+
+    class Meta:
+        db_table = "languages"
+
+    def clean(self):
+        instance = Languages.objects.filter(name=self.name)
 
         if instance.exists():
             raise AlreadyExistError(self)
--- backend/apps/teams/models.py
+++ backend/apps/teams/models.py
@@ -1,18 +1,19 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
 
 
 class Team(models.Model):
 
     TEAM_TYPES = {
-        ("A", "Autor"),
-        ("T", "Translator")
+        (1, _("Autor")),
+        (2, _("Translator"))
     }
 
     name = models.CharField(max_length=255)
     users = models.ManyToManyField(get_user_model())
     novels = models.ManyToManyField("novels.Novel")
-    team_type = models.CharField(max_length=1, choices=TEAM_TYPES, default="A")
+    team_type = models.IntegerField(choices=TEAM_TYPES, default=1)
     description = models.TextField(null=True)
 
     class Meta:
