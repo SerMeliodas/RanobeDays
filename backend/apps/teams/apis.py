7114437535658e37d492f153bc3f11282b6da48e
SerMeliodas <oliver9574@gmail.com>
2024-08-18 15:05:09 +0200

    refactor(teams): mini refactor pep

--- backend/apps/teams/apis.py
+++ backend/apps/teams/apis.py
@@ -1,5 +1,9 @@
 from rest_framework.views import APIView
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser, IsAuthenticated
+
+from rest_framework.permissions import (
+    IsAuthenticatedOrReadOnly, IsAdminUser, IsAuthenticated
+)
+
 from rest_framework.response import Response
 from rest_framework import status
 
