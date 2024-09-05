d869117601f59fcc621485df4f9273243fbc09e7
SerMeliodas <oliver9574@gmail.com>
2024-09-05 18:48:49 +0200

    feat(bookmarks): service test

--- backend/apps/bookmarks/tests/services/test_create_service.py
+++ backend/apps/bookmarks/tests/services/test_create_service.py
@@ -1,9 +1,22 @@
-from ..factories import BookmarkFactory
 from ...types import BookmarkObject
+from ...services import create_bookmark
+
+from apps.users.tests.factories import UserFactory
+from apps.chapters.tests.factories import ChapterFactory
 
 from unittest import TestCase
 
 
 class BookmarkCreateServiceTest(TestCase):
     def setUp(self):
-        ...
+        self.user = UserFactory()
+        self.chapter = ChapterFactory()
+
+    def test_create(self):
+        bookmark = create_bookmark(BookmarkObject(user=self.user,
+                                                  chapter=self.chapter.pk))
+
+        self.assertIsNotNone(bookmark)
+
+        self.assertEqual(bookmark.user, self.user)
+        self.assertEqual(bookmark.chapter, self.chapter)
